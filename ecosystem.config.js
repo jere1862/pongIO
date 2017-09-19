0c9f1be0032dafe1df9d863b1edc8e03229dda88
jere1862 <j-roy95@hotmail.com>
2017-09-19 18:02:57 -0400

    Correct ec2 address

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-13-58-226-192.us-east-2.compute.amazonaws.com',
+      host: 'ec2-13-59-169-82.us-east-2.compute.amazonaws.com',
       key: '../pongio-keypair.pem',
       ref: 'origin/master',
       repo: 'git@github.com:jere1862/pongIO.git',
